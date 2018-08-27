e675e07d5655badadb238bdac1fd0810c3bdb506
Douglas <msantos.douglas@gmail.com>
2018-08-27 17:19:43 -0300

    Create Bill Class (Model)
    * Add a Bill Model class for represent the Bill database table structure
    * Generate migration files for Bill Model
    * Register the Bill Model on Django Admin Site

--- telephone_call_api/bills/admin.py
+++ telephone_call_api/bills/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from bills.models import Bill
 # Register your models here.
+
+admin.site.register(Bill)
---
+++ telephone_call_api/bills/models/bill_model.py
@@ -0,0 +1,23 @@
+import uuid
+from datetime import timedelta
+
+from django.db import models
+from django.db.models import Count
+
+class Bill(models.Model):
+    """Represent a Bill data model
+
+    Contains the fields and business logic of a Bill entities.
+    Calculates the call prices and generate bill records.
+    
+    Attributes:
+        subscriber (mandatory): The subscriber of Bill (caller).
+        period (mandatory): The reference period of Bill (month/year).
+            format string = (mm/yyyy).
+    """
+
+    subscriber = models.CharField(max_length=11, blank=False, null=False)
+    period = models.CharField(max_length=7, blank=False, null=False)
+
+    def __str__(self):
+        return self.subscriber+' - '+self.period
