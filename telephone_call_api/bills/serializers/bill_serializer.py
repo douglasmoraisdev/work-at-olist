31f17e3211bf1985b03bbd96fa3c243877076c02
Douglas <msantos.douglas@gmail.com>
2018-08-28 15:24:24 -0300

    Format the call record output list of a Bill
    * Add the field 'call_records' on the output json of Bill endpoint

--- telephone_call_api/bills/serializers/bill_serializer.py
+++ telephone_call_api/bills/serializers/bill_serializer.py
@@ -12,6 +12,37 @@ class BillSerializer(serializers.ModelSerializer):
         exposed API endpoints.
     """
 
+    calls_records = serializers.SerializerMethodField()
+
     class Meta:
         model = Bill
-        fields = ('id', 'subscriber', 'period')
+        fields = ('id', 'subscriber', 'period', 'calls_records')
+
+    def get_calls_records(self, bill):
+        """Define the format for bill call records list output"""
+
+        _calls_records_list = []
+
+        # Get all bill records from the bill
+        for _recs in bill.billrecord_set.all():
+
+            # Calculate the call duration for output
+            _duration = _recs.end_call.timestamp - _recs.start_call.timestamp
+            _seconds = int(_duration.total_seconds() % 60)
+            _minutes = int(_duration.total_seconds() % 3600 // 60)
+            _hours = int(_duration.total_seconds() // 3600)
+
+            # Format the call duration for output
+            _duration_formated = '%sh%sm%ss' % (_hours, _minutes, _seconds)
+
+            # Format the call record and add to return list
+            _calls_records_data = {
+                'destination': _recs.end_call.destination,
+                'call_start': _recs.start_call.timestamp,
+                'call_end': _recs.end_call.timestamp,
+                'call_duration': _duration_formated,
+                'call_price': "R$%6.2f" % _recs.call_price
+            }
+            _calls_records_list.append(_calls_records_data)
+
+        return _calls_records_list
--- telephone_call_api/bills/test_views.py
+++ telephone_call_api/bills/test_views.py
@@ -1,14 +1,23 @@
+import time
+import datetime
+from datetime import timedelta
+import json
+
 from django.test import TestCase
 from rest_framework.test import APIClient
 
+from bills.models import Bill
+from billrecords.models import BillRecord
+from records.models import Record
 
-class TestBillEndPointsTestCase(TestCase):
+
+class BillEndPointsTestCase(TestCase):
     """Test Bill Calls Endpoints behavior"""
 
     def setUp(self):
         self.client = APIClient()
 
-    def test_bill_endpoint__options_200_response(self):
+    def test_bill_endpoint_options_200_response(self):
         """Make a OPTIONS request to Start Record Call endpoint
         Expect a response 200 response (success)
         """
@@ -52,3 +61,61 @@ class TestBillEndPointsTestCase(TestCase):
         request = self.client.delete('/bill/', {})
 
         self.assertEquals(request.status_code, 405)
+
+
+class BillFormatTestCase(TestCase):
+    """Test Bill Calls Endpoints behavior"""
+
+    def setUp(self):
+        self.client = APIClient()
+
+        # Create a Bill for tests
+        _start_call = Record()
+        _start_call.timestamp = datetime.datetime.strptime(
+                                  "2018-08-26T15:07:10+0000",
+                                  "%Y-%m-%dT%H:%M:%S%z")
+        _start_call.source = '51992657100'
+        _start_call.destination = '5133877079'
+        _start_call.call_type = 'S'
+        _start_call.call_id = '1'
+        _start_call.save()
+
+        _end_call = Record()
+        _end_call.timestamp = datetime.datetime.strptime(
+                                  "2018-08-26T15:17:10+0000",
+                                  "%Y-%m-%dT%H:%M:%S%z")
+        _end_call.call_type = 'E'
+        _end_call.call_id = '1'
+        _end_call.save()
+
+        _start_period = _start_call.timestamp
+        _end_period = _end_call.timestamp
+
+        _bill = Bill()
+        _bill.subscriber = '51992657100'
+        _bill.period = '06/2018'
+        _bill.save()
+
+        _bill_record = BillRecord()
+        _bill_record.bill_origin = _bill
+        _bill_record.start_call = _start_call
+        _bill_record.end_call = _end_call
+        _bill_record.call_price = _bill.calculate_charge(
+                                    _start_period, _end_period
+                                  )
+        _bill_record.save()
+
+    def test_bill_endpoint_fields(self):
+        """Test a output format (json) of a bill endpoint call"""
+
+        request = self.client.get('/bill/', {})
+
+        self.assertContains(request, 'id', status_code=200)
+        self.assertContains(request, 'subscriber', status_code=200)
+        self.assertContains(request, 'period', status_code=200)
+        self.assertContains(request, 'calls_records', status_code=200)
+        self.assertContains(request, 'destination', status_code=200)
+        self.assertContains(request, 'call_start', status_code=200)
+        self.assertContains(request, 'call_end', status_code=200)
+        self.assertContains(request, 'call_duration', status_code=200)
+        self.assertContains(request, 'call_price', status_code=200)
