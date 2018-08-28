92e2166c52e56599418375169ea3f2bb5b58aad5
Douglas <msantos.douglas@gmail.com>
2018-08-28 10:47:27 -0300

    Create calculate charge method for Bill Model
    * Add calculate_charge methos on Bill Model
    * Add bussiness rule or charge calls
    * Add tests for Bill bussiness rule

--- telephone_call_api/bills/models/bill_model.py
+++ telephone_call_api/bills/models/bill_model.py
@@ -17,8 +17,76 @@ class Bill(models.Model):
             format string = (mm/yyyy).
     """
 
+    STANDING_CHARGE = 0.36
+    NORMAL_CALL_CHARGE = 0.09
+    REDUCED_CALL_CHARGE = 0.0
+    REDUCED_TIME_BEGIN = 22
+    REDUCED_TIME_END = 6
     subscriber = models.CharField(max_length=11, blank=False, null=False)
     period = models.CharField(max_length=7, blank=False, null=False)
 
     def __str__(self):
         return self.subscriber+' - '+self.period
+
+    def calculate_charge(self, start_period, end_period):
+        """Calculate a call charge
+
+        Calculation Formula:
+            Call Charge =
+                Base charge
+                +(
+                    (call minute * Normal charge)
+                    + (call minute * Reduced charge)
+                )
+
+            Base charge (cost of a connection): STANDING_CHARGE
+            Normal charge (calls Between 6h and 22h): NORMAL_CALL_CHARGE
+            Reduced charge (calls Between 22h and 6h): REDUCED_CALL_CHARGE
+            For REDUCED_TIME_BEGIN = 22h and REDUCED_TIME_END = 6h
+
+            Example:
+            For a call started at 21:57:13 and finished at 22:10:56 we have:
+                Standing charge: R$ 0,36
+                Normal charge: R$ 0,09
+                Reduced charge: R$ 0,00
+                Call charge:
+                    Minutes in normal time between 21:57:13 and 22:00 = 2
+                    Price: 2 * R$ 0,09 = R$ 0,18
+                Total = R$ 0,36 + R$ 0,18 = 0,54
+        """
+
+        # Convert time charges on timedelta for simplify calculation
+        _start_reduced_time = timedelta(hours=self.REDUCED_TIME_BEGIN)
+        _end_reduced_time = timedelta(hours=self.REDUCED_TIME_END)
+
+        # Initialize variables for the time comparision loop
+        _normal_charge_minutes = 0
+        _reduced_charge_minutes = 0
+        _period_comparison = start_period + timedelta(minutes=1)
+
+        # Contabilize the minutes in normal time and/or reduced time
+        while (_period_comparison <= end_period):
+
+            _period_delta = timedelta(hours=_period_comparison.hour,
+                                      minutes=_period_comparison.minute,
+                                      seconds=_period_comparison.second)
+
+            # Verify the contabilized minute is in normal time or reduced time
+            if (_period_delta < _start_reduced_time) and \
+               (_period_delta >= _end_reduced_time+timedelta(minutes=1)):
+                _normal_charge_minutes += 1
+            else:
+                _reduced_charge_minutes += 1
+
+            # Go to next minute
+            _period_comparison += timedelta(minutes=1)
+
+        # Calculate the minutes charge
+        _total_charge = round(_normal_charge_minutes
+                              * self.NORMAL_CALL_CHARGE
+                              + _reduced_charge_minutes
+                              * self.REDUCED_CALL_CHARGE
+                              + self.STANDING_CHARGE,
+                              2)
+
+        return _total_charge
---
+++ telephone_call_api/bills/test_models.py
@@ -0,0 +1,223 @@
+import time
+import datetime
+from datetime import timedelta
+
+from django.test import TestCase
+
+from bills.models import Bill
+
+
+class CalculateCallTaxTestCase(TestCase):
+    """Test Bill calculate charge bussiness rule"""
+
+    def setUp(self):
+        pass
+
+    def test_normal_time_call_charge(self):
+        """
+        Test a 10 mins normal time call
+        """
+        ts_begin = datetime.datetime.strptime(
+            "2018-08-26 15:07:10", "%Y-%m-%d %H:%M:%S")
+        ts_end = datetime.datetime.strptime(
+            "2018-08-26 15:17:40", "%Y-%m-%d %H:%M:%S")
+
+        _one_minute_call_price = Bill().calculate_charge(ts_begin, ts_end)
+
+        self.assertEqual(_one_minute_call_price, 1.26)
+
+    def test_normal_time_call_charge_minus_1sec(self):
+        """
+        Test a 9mins 59sec call in normal time
+        """
+        ts_begin = datetime.datetime.strptime(
+            "2018-08-26 15:07:10", "%Y-%m-%d %H:%M:%S")
+        ts_end = datetime.datetime.strptime(
+            "2018-08-26 15:17:09", "%Y-%m-%d %H:%M:%S")
+
+        _one_minute_call_price = Bill().calculate_charge(ts_begin, ts_end)
+
+        self.assertEqual(_one_minute_call_price, 1.17)
+
+    def test_o_clock_to_reduced_22h_charge(self):
+        """
+        Test a 1hour 00sec call ended at begin reduced time o'clock
+        """
+        ts_begin = datetime.datetime.strptime(
+            "2018-08-26 21:00:00", "%Y-%m-%d %H:%M:%S")
+        ts_end = datetime.datetime.strptime(
+            "2018-08-26 22:00:00", "%Y-%m-%d %H:%M:%S")
+
+        _one_minute_call_price = Bill().calculate_charge(ts_begin, ts_end)
+
+        self.assertEqual(_one_minute_call_price, 1.17)
+
+    def test_o_clock_to_reduced_22h_charge(self):
+        """
+        Test a 10 mins call ended at the begin of a reduced time o'clock
+        """
+        ts_begin = datetime.datetime.strptime(
+            "2018-08-26 21:50:00", "%Y-%m-%d %H:%M:%S")
+        ts_end = datetime.datetime.strptime(
+            "2018-08-26 22:00:00", "%Y-%m-%d %H:%M:%S")
+
+        _one_minute_call_price = Bill().calculate_charge(ts_begin, ts_end)
+
+        self.assertEqual(_one_minute_call_price, 1.17)
+
+    def test_o_clock_to_reduced_22h_plus_1sec_charge(self):
+        """
+        Test a 10 mins call ended at the begin of a reduced time plus 1sec
+        """
+        ts_begin = datetime.datetime.strptime(
+            "2018-08-26 21:50:00", "%Y-%m-%d %H:%M:%S")
+        ts_end = datetime.datetime.strptime(
+            "2018-08-26 22:00:01", "%Y-%m-%d %H:%M:%S")
+
+        _one_minute_call_price = Bill().calculate_charge(ts_begin, ts_end)
+
+        self.assertEqual(_one_minute_call_price, 1.17)
+
+    def test_o_clock_to_reduced_6h_charge(self):
+        """
+        Test a 10 mins call ended at the end of a reduced time o'clock
+        """
+        ts_begin = datetime.datetime.strptime(
+            "2018-08-26 05:50:00", "%Y-%m-%d %H:%M:%S")
+        ts_end = datetime.datetime.strptime(
+            "2018-08-26 06:00:00", "%Y-%m-%d %H:%M:%S")
+
+        _one_minute_call_price = Bill().calculate_charge(ts_begin, ts_end)
+
+        self.assertEqual(_one_minute_call_price, 0.36)
+
+    def test_o_clock_to_reduced_6h_plus_1sec_charge(self):
+        """
+        Test a 10 mins call ended at the end of a reduced time
+            plus 1 sec
+        """
+        ts_begin = datetime.datetime.strptime(
+            "2018-08-26 05:50:00", "%Y-%m-%d %H:%M:%S")
+        ts_end = datetime.datetime.strptime(
+            "2018-08-26 06:00:01", "%Y-%m-%d %H:%M:%S")
+
+        _one_minute_call_price = Bill().calculate_charge(ts_begin, ts_end)
+
+        self.assertEqual(_one_minute_call_price, 0.36)
+
+    def test_o_clock_6h_plus_1min_charge(self):
+        """
+        Test a 1 min call ended at the end of a reduced call plus 1 min
+        """
+        ts_begin = datetime.datetime.strptime(
+            "2018-08-26 06:00:00", "%Y-%m-%d %H:%M:%S")
+        ts_end = datetime.datetime.strptime(
+            "2018-08-26 06:01:00", "%Y-%m-%d %H:%M:%S")
+
+        _one_minute_call_price = Bill().calculate_charge(ts_begin, ts_end)
+
+        self.assertEqual(_one_minute_call_price, 0.45)
+
+    def test_6h1s_plus_1min_charge(self):
+        """
+        Test a 59secs call started at 1 second after a end reduced time
+        """
+        ts_begin = datetime.datetime.strptime(
+            "2018-08-26 06:00:01", "%Y-%m-%d %H:%M:%S")
+        ts_end = datetime.datetime.strptime(
+            "2018-08-26 06:01:00", "%Y-%m-%d %H:%M:%S")
+
+        _one_minute_call_price = Bill().calculate_charge(ts_begin, ts_end)
+
+        self.assertEqual(_one_minute_call_price, 0.36)
+
+    def test_half_hour_to_reduced_charge(self):
+        """
+        Test a 40mins call started at 30mins before a reduced time
+        """
+        ts_begin = datetime.datetime.strptime(
+            "2018-08-26 21:30:10", "%Y-%m-%d %H:%M:%S")
+        ts_end = datetime.datetime.strptime(
+            "2018-08-26 23:10:10", "%Y-%m-%d %H:%M:%S")
+
+        _one_minute_call_price = Bill().calculate_charge(ts_begin, ts_end)
+
+        self.assertEqual(_one_minute_call_price, 2.97)
+
+    def test_2_mins_to_reduced_charge(self):
+        """
+        Test a 13 mins call started at 3 mins before a reduced time
+        """
+        ts_begin = datetime.datetime.strptime(
+            "2018-08-26 21:57:13", "%Y-%m-%d %H:%M:%S")
+        ts_end = datetime.datetime.strptime(
+            "2018-08-26 23:10:56", "%Y-%m-%d %H:%M:%S")
+
+        _one_minute_call_price = Bill().calculate_charge(ts_begin, ts_end)
+
+        self.assertEqual(_one_minute_call_price, 0.54)
+
+    def test_cross_from_reduced_to_charge(self):
+        """
+        Test a call started on a reduced time and ended at normal time
+        """
+        ts_begin = datetime.datetime.strptime(
+            "2018-08-25 23:25:10", "%Y-%m-%d %H:%M:%S")
+        ts_end = datetime.datetime.strptime(
+            "2018-08-26 10:15:10", "%Y-%m-%d %H:%M:%S")
+
+        _one_minute_call_price = Bill().calculate_charge(ts_begin, ts_end)
+
+        self.assertEqual(_one_minute_call_price, 23.31)
+
+    def test_24h_call_charge(self):
+        """
+        Test a 24 hours call
+        """
+        ts_begin = datetime.datetime.strptime(
+            "2018-08-26 14:58:08", "%Y-%m-%d %H:%M:%S")
+        ts_end = datetime.datetime.strptime(
+            "2018-08-27 14:58:08", "%Y-%m-%d %H:%M:%S")
+
+        _one_minute_call_price = Bill().calculate_charge(ts_begin, ts_end)
+
+        self.assertEqual(_one_minute_call_price, 86.67)
+
+    def test_48h_call_charge(self):
+        """
+        Test a 48 hours call
+        """
+        ts_begin = datetime.datetime.strptime(
+            "2018-08-26 14:58:08", "%Y-%m-%d %H:%M:%S")
+        ts_end = datetime.datetime.strptime(
+            "2018-08-28 14:58:08", "%Y-%m-%d %H:%M:%S")
+
+        _one_minute_call_price = Bill().calculate_charge(ts_begin, ts_end)
+
+        self.assertEqual(_one_minute_call_price, 172.98)
+
+    def test_reduced_time_call_charge(self):
+        """
+        Test a call that started and ended into a reduced time
+        """
+        ts_begin = datetime.datetime.strptime(
+            "2018-08-26 22:00:00", "%Y-%m-%d %H:%M:%S")
+        ts_end = datetime.datetime.strptime(
+            "2018-08-27 06:00:00", "%Y-%m-%d %H:%M:%S")
+
+        _one_minute_call_price = Bill().calculate_charge(ts_begin, ts_end)
+
+        self.assertEqual(_one_minute_call_price, 0.36)
+
+    def test_max_normal_time_call_in_a_day_charge(self):
+        """
+        Test a call that started and ended into a normal time
+        """
+        ts_begin = datetime.datetime.strptime(
+            "2018-08-26 06:00:00", "%Y-%m-%d %H:%M:%S")
+        ts_end = datetime.datetime.strptime(
+            "2018-08-26 22:00:00", "%Y-%m-%d %H:%M:%S")
+
+        _one_minute_call_price = Bill().calculate_charge(ts_begin, ts_end)
+
+        self.assertEqual(_one_minute_call_price, 86.67)
