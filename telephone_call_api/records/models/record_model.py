ddd2427d2918e223c8156a7fb867d5e6debae0b3
Douglas <msantos.douglas@gmail.com>
2018-08-28 14:15:30 -0300

    Create save Call Record Business rule
    * Override the Record Model save method
    * The save method check consistency and generate call charges

--- telephone_call_api/records/models/record_model.py
+++ telephone_call_api/records/models/record_model.py
@@ -4,6 +4,7 @@ from django.db import models
 from rest_framework.exceptions import NotFound, ValidationError
 
 from bills.models import Bill
+from billrecords.models import BillRecord
 
 
 class Record(models.Model):
@@ -30,7 +31,7 @@ class Record(models.Model):
         (END_CALL_TYPE, 'End')
     )
 
-    call_type = models.CharField(max_length=1, blank=False, 
+    call_type = models.CharField(max_length=1, blank=False,
                                  null=False, choices=TYPE_CALL_CHOICES)
     timestamp = models.DateTimeField(blank=False, null=False)
     call_id = models.IntegerField(blank=False, unique=False, null=False)
@@ -41,3 +42,83 @@ class Record(models.Model):
 
     def __str__(self):
         return self.source+' - '+self.call_type+' - '+str(self.call_id)
+
+    def save(self, *args, **kwargs):
+        """Business rule for save a new Start or End call record
+
+        Override the Record Save Method for implement business rule
+        Verify Call Record consistency:
+            - Only valid Call Record types are allowed:
+                (START_CALL_TYPE or END_CALL_TYPE)
+            - Only unique call_id per call type is allowed
+            - A E type Call record only may exist if a origin S type
+                Call record (same call_id) already exists
+            - The end call date/time MAY be higher than
+                the start call date/time
+        """
+
+        # Verify Call Type
+        _valid_type = False
+        for choise in self.TYPE_CALL_CHOICES:
+            if self.call_type == choise[0]:
+                _valid_type = True
+
+        if not _valid_type:
+            raise ValidationError({'detail': 'Invalid call record Type'})
+
+        # Verify a call with same type and call_id already created
+        if Record.objects.filter(call_id=self.call_id,
+                                 call_type=self.call_type).exists():
+            raise ValidationError(
+                {'detail': 'This record call_id is already created'})
+
+        # If a End Call record, generate a charge
+        if self.call_type == self.END_CALL_TYPE:
+
+            # Get the origin Start Call
+            _origin_start_call = Record.objects.filter(
+                call_id=self.call_id, call_type=self.START_CALL_TYPE)
+
+            if not _origin_start_call.exists():
+                raise NotFound('Origin start call record not Found')
+
+            # Verify the Start and End date/time consistency
+            if _origin_start_call.get().timestamp > self.timestamp:
+                raise ValidationError(
+                                      {'detail': 'Date/time from origin \
+                                      start call is higher then end call'})
+
+            # Get the period string
+            _end_call_period = self.timestamp.strftime("%m/%Y")
+
+            self.source = _origin_start_call.get().source
+            self.destination = _origin_start_call.get().destination
+
+            # Get a already created bill for the period
+            _already_created_bill = Bill.objects.filter(
+                                                        period=_end_call_period
+                                                        )
+            if _already_created_bill.exists():
+                self.bill = _already_created_bill.get()
+
+            # If not found a already created Bill, create a new one
+            else:
+                _new_created_bill = Bill()
+                _new_created_bill.subscriber = self.source
+                _new_created_bill.period = _end_call_period
+                _new_created_bill.save(self)
+
+                self.bill = _new_created_bill
+
+            # save the information of bill on the origin
+            # start call record
+            _origin_start_call.update(bill=self.bill)
+
+            # save the current end call record
+            super(Record, self).save(*args, **kwargs)
+
+            # update the bill
+            Bill().update_bill_record(_origin_start_call.get(), self)
+
+        else:
+            super(Record, self).save(*args, **kwargs)
--- telephone_call_api/records/tests_models.py
+++ telephone_call_api/records/tests_models.py
@@ -2,6 +2,8 @@ import datetime
 
 from django.test import TestCase
 from rest_framework.test import APIClient
+from rest_framework.exceptions import NotFound, ValidationError
+
 
 from records.models import Record
 
@@ -9,10 +11,8 @@ from records.models import Record
 class TestRecordModels(TestCase):
     """Test Record Calls Models behavior"""
 
-    def test_start_record_insertion(self):
-        """Make a INSERT into Record table (model)
-        Expect a new row added (success)
-        """
+    def test_save_record_S(self):
+        """Save a S type call record"""
 
         _srecord = Record()
         _srecord.timestamp = datetime.datetime.strptime(
@@ -20,9 +20,165 @@ class TestRecordModels(TestCase):
                                   "%Y-%m-%dT%H:%M:%S%z")
         _srecord.source = '51992657100'
         _srecord.destination = '5133877079'
-        _srecord.type = 'S'
+        _srecord.call_type = 'S'
         _srecord.call_id = '1'
 
         _srecord.save()
 
         self.assertEquals(Record.objects.filter(call_id=1).count(), 1)
+
+    def test_save_record_E(self):
+        """Save a E type call record"""
+
+        _srecord = Record()
+        _srecord.timestamp = datetime.datetime.strptime(
+                                    "2018-08-26T15:07:10+0000",
+                                    "%Y-%m-%dT%H:%M:%S%z")
+        _srecord.source = '51992657100'
+        _srecord.destination = '5133877079'
+        _srecord.call_type = 'S'
+        _srecord.call_id = '1'
+
+        _srecord.save()
+
+        _erecord = Record()
+        _erecord.timestamp = datetime.datetime.strptime(
+                                        "2018-08-26T16:16:13+0000",
+                                        "%Y-%m-%dT%H:%M:%S%z")
+        _erecord.call_type = 'E'
+        _erecord.call_id = '1'
+
+        _erecord.save()
+
+        self.assertEquals(Record.objects.filter(call_id=1).count(), 2)
+
+    def test_save_record_E_without_S_origin(self):
+        """Save a E type call record without a S origin
+        Expect fail (NotFound exception)
+        """
+
+        _srecord = Record()
+        _srecord.timestamp = datetime.datetime.strptime(
+                                                "2018-08-26T15:07:10+0000",
+                                                "%Y-%m-%dT%H:%M:%S%z")
+        _srecord.source = '51992657100'
+        _srecord.destination = '5133877079'
+        _srecord.call_type = 'S'
+        _srecord.call_id = '1'
+
+        _srecord.save()
+
+        _ierecord = Record()
+        _ierecord.timestamp = datetime.datetime.strptime(
+                                                "2018-08-26T16:16:13+0000",
+                                                "%Y-%m-%dT%H:%M:%S%z")
+        _ierecord.call_type = 'E'
+        _ierecord.call_id = '2'
+
+        self.assertRaises(NotFound, lambda: _ierecord.save())
+        self.assertNotEquals(Record.objects.filter(call_id=1).count(), 2)
+
+    def test_save_record_Not_E_or_S(self):
+        """Save a X type call record
+        Expect fail (NotFound exception)
+        """
+
+        _srecord = Record()
+        _srecord.timestamp = datetime.datetime.strptime(
+                                                "2018-08-26T15:07:10+0000",
+                                                "%Y-%m-%dT%H:%M:%S%z")
+        _srecord.source = '51992657100'
+        _srecord.destination = '5133877079'
+        _srecord.call_type = 'X'
+        _srecord.call_id = '1'
+
+        self.assertRaises(ValidationError, lambda: _srecord.save())
+
+    def test_save_record_E_lower_time_S(self):
+        """Save a E type call record with timestamp lower than
+        the S type call origin
+
+        Expect fail (ValidationError exception)
+        """
+
+        _srecord = Record()
+        _srecord.timestamp = datetime.datetime.strptime(
+                                                "2018-08-26T15:07:10+0000",
+                                                "%Y-%m-%dT%H:%M:%S%z")
+        _srecord.source = '51992657100'
+        _srecord.destination = '5133877079'
+        _srecord.call_type = 'S'
+        _srecord.call_id = '1'
+
+        _srecord.save()
+
+        _ierecord = Record()
+        _ierecord.timestamp = datetime.datetime.strptime(
+                                                "2018-08-26T14:16:13+0000",
+                                                "%Y-%m-%dT%H:%M:%S%z")
+        _ierecord.call_type = 'E'
+        _ierecord.call_id = '1'
+
+        self.assertRaises(ValidationError, lambda: _ierecord.save())
+        self.assertNotEquals(Record.objects.filter(call_id=1).count(), 2)
+
+    def test_save_record_E_already_closed(self):
+        """Save a E type call record for a already ended call pair
+        Expect fail (ValidationError exception)
+        """
+
+        _srecord = Record()
+        _srecord.timestamp = datetime.datetime.strptime(
+                                                "2018-08-26T15:07:10+0000",
+                                                "%Y-%m-%dT%H:%M:%S%z")
+        _srecord.source = '51992657100'
+        _srecord.destination = '5133877079'
+        _srecord.call_type = 'S'
+        _srecord.call_id = '1'
+        _srecord.save()
+
+        _ierecord = Record()
+        _ierecord.timestamp = datetime.datetime.strptime(
+                                                 "2018-08-26T16:16:13+0000",
+                                                 "%Y-%m-%dT%H:%M:%S%z")
+        _ierecord.call_type = 'E'
+        _ierecord.call_id = '1'
+        _ierecord.save()
+
+        _ierecord_2 = Record()
+        _ierecord_2.timestamp = datetime.datetime.strptime(
+                                                    "2018-08-26T16:26:13+0000",
+                                                    "%Y-%m-%dT%H:%M:%S%z")
+        _ierecord_2.call_type = 'E'
+        _ierecord_2.call_id = '1'
+
+        self.assertRaises(ValidationError, lambda: _ierecord_2.save())
+        self.assertNotEquals(Record.objects.filter(call_id=1).count(), 3)
+
+    def test_save_record_S_already_closed(self):
+        """Save a S type call record for a already created
+        S call record with same call_id
+        Expect fail (ValidationError exception)
+        """
+
+        _srecord_1 = Record()
+        _srecord_1.timestamp = datetime.datetime.strptime(
+                                                    "2018-08-26T15:07:10+0000",
+                                                    "%Y-%m-%dT%H:%M:%S%z")
+        _srecord_1.source = '51992657100'
+        _srecord_1.destination = '5133877079'
+        _srecord_1.call_type = 'S'
+        _srecord_1.call_id = '1'
+        _srecord_1.save()
+
+        _srecord_2 = Record()
+        _srecord_2.timestamp = datetime.datetime.strptime(
+                                                    "2018-08-28T15:07:10+0000",
+                                                    "%Y-%m-%dT%H:%M:%S%z")
+        _srecord_2.source = '51992657100'
+        _srecord_2.destination = '51994355677'
+        _srecord_2.call_type = 'S'
+        _srecord_2.call_id = '1'
+
+        self.assertRaises(ValidationError, lambda: _srecord_2.save())
+        self.assertNotEquals(Record.objects.filter(call_id=1).count(), 2)
